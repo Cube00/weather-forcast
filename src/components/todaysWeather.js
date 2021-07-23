e274e5245cb886c35c9728aa5cf277bd6220a735
Your Name <coller.vato@gmail.com>
2021-07-23 08:40:56 +0400

    rewrite days

--- src/components/todaysWeather.js
+++ src/components/todaysWeather.js
@@ -26,14 +26,7 @@ const TodaysWeather = ({ state }) => {
 
   //get week days
   const defaultData = new Date();
-  const array = new Array(7);
-  array[0] = 'Sunday';
-  array[1] = 'Monday';
-  array[2] = 'Tuesday';
-  array[3] = 'Wednesday';
-  array[4] = 'Thusday';
-  array[5] = 'Friday';
-  array[6] = 'Saturday';
+  const array = ['Sunday','Monday','Tuesday','Wednesday','Thusday','Friday','Saturday'];
 
   const weekDay = array[defaultData.getDay()];
 
